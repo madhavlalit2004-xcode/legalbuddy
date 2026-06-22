c761b3077a9e5cbee4b442bb673564d7fd28cc51
madhavlalit2004-xcode <madhavlalit2004@gmail.com>
2026-06-22 18:51:02 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 from app.core.exceptions import LegalBuddyException, legal_buddy_exception_handler
 from app.core.logging import configure_logging
 
+configure_logging()
+
 app = FastAPI(
     title=settings.APP_NAME, 
     version=settings.APP_VERSION,
     debug=settings.DEBUG
 )
 app.add_exception_handler(LegalBuddyException, legal_buddy_exception_handler)
-
-configure_logging()
